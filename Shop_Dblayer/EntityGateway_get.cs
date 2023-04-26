3031419488296962f2b76c0ab65992edfa678b3f
Ксения <@DESKTOP-G0L8SDH>
2023-04-26 19:43:52 +0300

    Finished

--- Shop_Dblayer/EntityGateway_get.cs
+++ Shop_Dblayer/EntityGateway_get.cs
@@ -10,6 +10,23 @@ namespace Shop_dblayer
     {
         public IEnumerable<Product> GetProducts(Func<Product, bool> predicate) =>
             ShopContext.Products.Where(predicate).ToArray();
+        public IEnumerable<Product> GetProducts() =>
+            GetProducts(x => true);
+
+        public IEnumerable<Purchase> GetPurchases(Func<Purchase, bool> predicate) =>
+            ShopContext.Purchases.Where(predicate).ToArray();
+        public IEnumerable<Purchase> GetPurchases() =>
+            GetPurchases(x => true);
+
+        public IEnumerable<Client> GetClients(Func<Client, bool> predicate) =>
+            ShopContext.Clients.Where(predicate).ToArray();
+        public IEnumerable<Client> GetClients() =>
+            GetClients (x => true);
+
+        public IEnumerable<Provider> GetProviders(Func<Provider, bool> predicate) =>
+            ShopContext.Providers.Where(predicate).ToArray();
+        public IEnumerable<Provider> GetProviders() =>
+            GetProviders(x => true);
         
     }
 }
